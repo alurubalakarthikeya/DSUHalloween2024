03d4f58fc8db61ca01a593b38d5eb41678283533
Bala Karthikeya Aluru <alurubalakarthikeya@gmail.com>
2024-09-14 23:35:46 +0530

    minor-changes

--- main.js
+++ main.js
@@ -54,6 +54,14 @@ const sr = ScrollReveal({
     reset: true
 })
 
+sr.reveal(`.home__data, .footer__content, .footer__logo, .footer__description`)
+sr.reveal(`.home__tree-1`, {origin: 'left', delay:800})
+sr.reveal(`.home__tree-2`, {origin: 'right', delay:800})
+sr.reveal(`.home__img`, {delay: 800})
+sr.reveal(`.category__card, .items__card`, {interval: 100})
+sr.reveal(`.about__img, .about__data, .footer__tree-2`, {origin: 'left'})
+sr.reveal(`.party__images, .party__data, .footer__tree-1`, {origin: 'right'})
+
 let mouseX = 0;
 let mouseY = 0;
 
